25d6d8933d01075188ed0dc2087ec02bad3d94e1
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-01-30 19:39:15 +0900

    Fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ctrl = {
     menu: (open, close, content) => {
         document.querySelector(open).addEventListener("click", () => {
-            document.querySelector("a").setAttribute("style", "display: block;opacity: 0.8;transition-property: opacity;transition-duration: 1s")
+            document.querySelector(open).setAttribute("style", "display: block;opacity: 0.8;transition-property: opacity;transition-duration: 1s")
         });
         document.querySelector(close).addEventListener("click", () => {
             setTimeout(() => {
@@ -11,13 +11,19 @@ const ctrl = {
         })
     },
     loadProfile: (name) => {
-
+        fetch(``)
     },
     loadPost: (author, dir) => {
+        // 一応
+        if (dir.search("?") !== -1 || dir.search("#") !== -1) return document.querySelector("main").innerHTML = "[Detect Danger] contains # or ?";
         fetch(`https://raw.githubusercontent.com/${author}/community_post/main/content/${dir}.md`)
             .then(res => res.text())
             .then(data => {
+                if (data == "404: Not Found") return document.querySelector("main").innerHTML = "404: Not Found";
                 document.querySelector("main").innerHTML = markdown.parse(data);
+            })
+            .catch(() => {
+                document.querySelector("main").innerHTML = "Network Error";
             })
     }
 }
@@ -25,24 +31,24 @@ const ctrl = {
 window.addEventListener("DOMContentLoaded", () => {
     markdown.ready;
     ctrl.menu(".menu", ".close", ".inMenu");
+    let info = "";
 
-    if (!(location.pathname.substring(1).split("/").length > 2)) {
-        switch (location.pathname.substring(1).split("/").length) {
-            case 0:
-                loadPost("blogbooks", "welcome");
-                break;
-            case 1:
-
-                break;
-            case 2:
-                
-                break;
-        
-            default:
-                loadPost("blogbooks", "error");
-                break;
-        }
+    if (location.pathname.endsWith("/")) {
+        info = location.pathname.substring(1, location.pathname.length - 1);
     } else {
-        
+        info = location.pathname.substring(1);
+    }
+
+    switch (info.split("/").length) {
+        case 0:
+            loadPost("blogbooks", ".views/welcome");
+            break;
+        case 1:
+            loadProfile(info.substring(1).split("/")[0]);
+            break;
+
+        default:
+            loadPost(info.substring(1).split("/")[0], info.substring(info.indexOf(location.pathname.substring(1).split("/")[1])));
+            break;
     }
 })
