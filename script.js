b8314b42095cea061a27eef18c6f94ad2e3af5d0
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-01-30 20:15:52 +0900

    Fix if at get access info: URL

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ ctrl.menu(".menu", ".close", ".inMenu");
 
 // get access info: URL
 let info = "";
-if (location.pathname.endsWith("/") && info !== "/") {
+if (location.pathname.endsWith("/") && location.pathname !== "/") {
     info = location.pathname.substring(1, location.pathname.length - 1);
 } else {
     info = location.pathname.substring(1);
